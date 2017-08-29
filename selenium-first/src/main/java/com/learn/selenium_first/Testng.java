b2e847867f201e589d8ee3abca368a4ec047f549
Piyush Kumar Goyal <piyush.kumar.goyal@accenture.com>
2017-08-29 20:59:49 +0100

    adding working set

---
+++ selenium-first/src/main/java/com/learn/selenium_first/Testng.java
@@ -0,0 +1,30 @@
+package com.learn.selenium_first;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+
+public class Testng {
+	
+	WebDriver driver;
+
+	@Test
+	public void f() {
+		driver.get("https://www.google.com");
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		System.setProperty("webdriver.chrome.driver","./webdriver/chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+
+	@AfterTest
+	public void afterTest() {
+		System.out.println("ALlLpass");
+	}
+
+}
